c1a6ceb199b883f5e54418b63a54430f8c43f6ca
stank2130 <stank2130@gmail.com>
2026-05-28 22:56:16 +0800

    Update page.js

--- app/article/[slug]/page.js
+++ app/article/[slug]/page.js
@@ -27,7 +27,7 @@ function formatDate(dateStr) {
 
 function getYoutubeId(url) {
   if (!url) return null
-  const match = url.match(/(?:youtube\.com\/watch\?v=|youtu\.be\/)([^&\n?#]+)/)
+  const match = url.match(/(?:youtube\.com\/watch\?v=|youtu\.be\/|youtube\.com\/shorts\/)([^&\n?#]+)/)
   return match ? match[1] : null
 }
 
